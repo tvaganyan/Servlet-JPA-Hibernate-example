5c270282558fb1a705177195ced5e27e18f92762
Tigran Vaganyan <tvaganyan@yandex.ru>
2022-07-03 05:33:15 +0300

    Update LotteryStart.java

--- app/src/main/java/lottery/service/LotteryStart.java
+++ app/src/main/java/lottery/service/LotteryStart.java
@@ -15,20 +15,7 @@ public class LotteryStart {
     public int getRandomNumber(long n) {
         return (int) ((Math.random() * (n - 1)) + 1);
     }
-
-    public int getRandomNumberHTTPS(long n) throws IOException {
-        String httpsURL = "https://www.random.org/integers/?num=1&min=1&max="
-                + n + "&col=1&base=10&format=plain&rnd=new";
-        URL myUrl = new URL(httpsURL);
-        HttpsURLConnection conn = (HttpsURLConnection)myUrl.openConnection();
-        InputStream is = conn.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        int r = Integer.parseInt(br.readLine(), 10);
-        System.out.println(r);
-        return r;
-    }
-
+    
     public JSONObject Start()throws IOException{
         JSONObject o = new JSONObject();
         long count = actions.ParticipantCount();
